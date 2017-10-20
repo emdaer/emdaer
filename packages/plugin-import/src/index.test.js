dc9a235393a2304897d0dcdb6c9aec004861d509
Luke Herrington <lherrington100@gmail.com>
2017-10-20 11:21:02 -0700

    Add option to plugin-import to run emdaer on imported file. Closes #12

--- packages/plugin-import/src/index.test.js
+++ packages/plugin-import/src/index.test.js
@@ -14,4 +14,20 @@ describe('@emdaer/plugin-import', () => {
       'This is the water and this is the well. Drink full and descend. The horse is the white of the eyes and dark within.'
     );
   });
+
+  test('imports a file and runs emdaer on it', async () => {
+    fs.readFile.mockImplementation(
+      async () =>
+        `This is the water and this is the well. Drink full and descend. The horse is the white of the eyes and dark within.
+<!--emdaer-p
+  - '@emdaer/plugin-value-from-package'
+  - value: name
+-->`
+    );
+    expect(
+      await importPlugin({ path: './tmp/.DS_Store', runEmdaer: true })
+    ).toBe(
+      'This is the water and this is the well. Drink full and descend. The horse is the white of the eyes and dark within.\nemdaer'
+    );
+  });
 });
