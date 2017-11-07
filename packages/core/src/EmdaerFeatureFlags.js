30a230b415e6921d33f3712cca9f6936935b0103
flipactual <flipactual@gmail.com>
2017-11-06 21:02:48 -0600

    feat(core): add feature flags

--- packages/core/src/EmdaerFeatureFlags.js
+++ packages/core/src/EmdaerFeatureFlags.js
@@ -1,4 +1,11 @@
-const EmdaerFeatureFlags = {
+/* @flow */
+
+type FeatureFlags = {|
+  enableASTParsing: boolean,
+  enableCommonComment: boolean,
+|};
+
+const EmdaerFeatureFlags: FeatureFlags = {
   // Experimental AST parsing of Markdown files:
   enableASTParsing: false,
   // Experimental single comment type:
