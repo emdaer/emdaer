45b72ec9e1e56488ebd8b0ec844569c939d4d810
Luke Herrington <lherrington100@gmail.com>
2018-01-14 17:06:50 -0800

    chore: use travis build stages

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,43 +1,31 @@
 #!/bin/bash
 set -e
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "We are in a pull request, not releasing"
-  exit 0
-fi
+rm -rf .git
+git init
+git clean -dfx
+git remote add origin https://github.com/emdaer/emdaer.git
+git fetch origin
+git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
+git checkout $TRAVIS_BRANCH
 
-if [[ $TRAVIS_BRANCH != 'master' ]]; then
-  echo "Builds from a branch are not released to NPM"
-  exit 0
-fi
+git config credential.helper store
+echo "https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/emdaer/emdaer.git" > ~/.git-credentials
 
-if [[ $TRAVIS_BRANCH == 'master' ]]; then
-  rm -rf .git
-  git init
-  git clean -dfx
-  git remote add origin https://github.com/emdaer/emdaer.git
-  git fetch origin
-  git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
-  git checkout $TRAVIS_BRANCH
+npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+npm prune
 
-  git config credential.helper store
-  echo "https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/emdaer/emdaer.git" > ~/.git-credentials
+git config --global user.email "${RELEASE_GH_EMAIL}"
+git config --global user.name "${RELEASE_GH_USERNAME}"
+git config --global push.default simple
 
-  npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
-  npm prune
+git fetch --tags
+git branch -u origin/$TRAVIS_BRANCH
+git fsck --full #debug
+echo "npm whoami"
+npm whoami #debug
+echo "git config --list"
+git config --list #debug
 
-  git config --global user.email "${RELEASE_GH_EMAIL}"
-  git config --global user.name "${RELEASE_GH_USERNAME}"
-  git config --global push.default simple
-
-  git fetch --tags
-  git branch -u origin/$TRAVIS_BRANCH
-  git fsck --full #debug
-  echo "npm whoami"
-  npm whoami #debug
-  echo "git config --list"
-  git config --list #debug
-
-  yarn
-  lerna publish --conventional-commits --yes --force-publish=* --message="[skip ci] Bump lerna version"
-fi
+yarn
+lerna publish --conventional-commits --yes --force-publish=* -m "chore: publish %s"
