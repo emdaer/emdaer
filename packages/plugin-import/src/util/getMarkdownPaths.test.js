a541d25414d8aae1f501ef58c9b9fdef6787c60c
flipactual <flipactual@gmail.com>
2019-07-08 12:58:38 -0500

    test: fix test for real
    Signed-off-by: flipactual <flipactual@gmail.com>

--- packages/plugin-import/src/util/getMarkdownPaths.test.js
+++ packages/plugin-import/src/util/getMarkdownPaths.test.js
@@ -5,19 +5,12 @@ describe('@emdaer/plugin-import', () => {
     jest.resetModules();
   });
   it('returns array of paths', () => {
-    require.main = { paths: ['/Users/test/code/emdaer/node_modules'] };
+    jest.mock('process', () => ({ cwd: () => [] }));
+    require.main.paths = ['/Users/test/code/emdaer/node_modules'];
     jest.spyOn(process, 'cwd').mockReturnValue('/Users/test/code/emdaer');
     expect(getMarkdownPaths('path')).toEqual([
       '/Users/test/code/emdaer/path',
-      '/home/flip/Development/emdaer/packages/plugin-import/src/util/node_modules/path',
-      '/home/flip/Development/emdaer/packages/plugin-import/src/node_modules/path',
-      '/home/flip/Development/emdaer/packages/plugin-import/node_modules/path',
-      '/home/flip/Development/emdaer/packages/node_modules/path',
-      '/home/flip/Development/emdaer/node_modules/path',
-      '/home/flip/Development/node_modules/path',
-      '/home/flip/node_modules/path',
-      '/home/node_modules/path',
-      '/node_modules/path',
+      '/Users/test/code/emdaer/node_modules/path',
     ]);
   });
 });
