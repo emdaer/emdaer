933a59947eed0f3fa75301db667a395c5c8a121d
flipactual <flipactual@gmail.com>
2020-04-09 16:36:14 -0500

    fix: butter sock
    Signed-off-by: flipactual <flipactual@gmail.com>

--- packages/core/src/util/EmdaerError.js
+++ packages/core/src/util/EmdaerError.js
@@ -1,13 +1,10 @@
-/* @flow */
-
 /**
  * @class
  * A custom error class which has a `code` property that
  * should align with emdaer errors defined in ../errors.js
  */
 class EmdaerError extends Error {
-  code: string;
-  constructor(code: string, ...args: any) {
+  constructor(code, ...args) {
     const [message, ...rest] = args;
     super(message || code, ...rest);
 
